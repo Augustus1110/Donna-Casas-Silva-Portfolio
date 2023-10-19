0f5861fe3b0b764e4e923c3bfe362c236712c9b1
Augustus1110 <donahi22@gmail.com>
2023-10-19 00:24:09 -0700

    Wrote and styled the About Me section

--- src/components/About.js
+++ src/components/About.js
@@ -17,10 +17,14 @@ function About() {
     <div className="App">
         <h2>About Me</h2>
         <img className="powerpuff" src={ImgProfile} alt ="Powerpuff Donna" />
-      <h3>Recent UC Berkeley Extension Coding Bootcamp graduate embarking on new Web Development career.</h3>
-  
-    
+      <p>My name is Donna and I am a recent graduate of the UC Berkeley Extension Coding Bootcamp.</p> 
+      <p>I am based in the San Francisco Bay Area and with my newly acquired range of front-end and back-end development skills, I am embarking on a new career in web development.</p> 
+      <p>My front-end proficiencies include HTML, CSS, JavaScript, jQuery, responsive design, React, and Bootstrap.</p> 
+      <p>My back-end proficiencies include APIs, Node, Express, MySQL, Sequelize, MongoDB, Mongoose, REST, and GraphQL.</p> 
+      <p>With this skill set, I am well-equipped to work on a range of web development projects and contribute to the development of dynamic and engaging web applications.</p>
     </div>
+
+
 
 
     <div style={{ height: '700px' }} />
